66eb6eaa1e6f2c18880ad07d40b0d0d5ed598f43
nirajchand <149762449+nirajchand@users.noreply.github.com>
2025-12-02 22:13:37 +0545

    adding first screen of app

---
+++ lib/screens/choose_role_screen.dart
@@ -0,0 +1,51 @@
+import 'package:farm_express/widgets/my_container.dart';
+import 'package:flutter/material.dart';
+
+class ChooseRoleScreen extends StatelessWidget {
+  const ChooseRoleScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          SizedBox(height: 20),
+          Center(child: Image.asset("assets/images/project_logo.png")),
+          SizedBox(height: 80),
+          Text(
+            "Choose Your Role",
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 30),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              MyContainer(
+                height: 200,
+                width: 170,
+                role: Text(
+                  "I'm\nfarmer",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+                icon: Image.asset("assets/icons/primary.png"),
+              ),
+              MyContainer(
+                height: 200,
+                width: 170,
+                role: Text(
+                  "I'm\nconsumer",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+                icon: Image.asset("assets/icons/home.png"),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
