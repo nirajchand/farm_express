abf18dccc300ed5695b5ee46f2af51ab154eadfc
nirajchand <149762449+nirajchand@users.noreply.github.com>
2025-12-31 23:03:21 +0545

    adding auth entity

---
+++ lib/features/auth/domain/entities/auth_entity.dart
@@ -0,0 +1,17 @@
+import 'package:equatable/equatable.dart';
+
+class AuthEntity extends Equatable {
+  final String? userId;
+  final String email;
+  final String? password;
+  final String? confirmPassword;
+
+  const AuthEntity({
+    this.userId,
+    required this.email,
+    this.password,
+    this.confirmPassword,
+  });
+  @override
+  List<Object?> get props => [userId,email,password,confirmPassword];
+}
