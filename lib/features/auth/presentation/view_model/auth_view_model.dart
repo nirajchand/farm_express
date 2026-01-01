5a1246df698389771c5fef944cfcd285ea46a7e7
nirajchand <149762449+nirajchand@users.noreply.github.com>
2026-01-01 14:22:47 +0545

    adding viewmodels

---
+++ lib/features/auth/presentation/view_model/auth_view_model.dart
@@ -0,0 +1,72 @@
+import 'package:farm_express/features/auth/domain/usecases/login_usecases.dart';
+import 'package:farm_express/features/auth/domain/usecases/register_usecases.dart';
+import 'package:farm_express/features/auth/presentation/state/auth_state.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final authViewModelProvider =
+    NotifierProvider<AuthViewModel, AuthState>(() => AuthViewModel());
+
+
+class AuthViewModel extends Notifier<AuthState> {
+  late final RegisterUsecase _registerUsecase;
+  late final LoginUsecase _loginUsecases;
+
+  @override
+  AuthState build() {
+    _registerUsecase = ref.read(registerUsecaseProvider);
+    _loginUsecases = ref.read(loginUsecasesProvider);
+    return AuthState();
+  }
+
+  // Register
+  Future<void> register({
+    required String fullName,
+    required String email,
+    required String password,
+    required String confirmPassword,
+  }) async {
+    state = state.copyWith(status: AuthStatus.loading);
+    final params = RegisterUsecasesParam(
+      fullName: fullName,
+      email: email,
+      password: password,
+      confirmPassword: confirmPassword,
+    );
+    final result = await _registerUsecase.call(params);
+    result.fold(
+      (failure) {
+        state = state.copyWith(
+          status: AuthStatus.error,
+          errorMessage: 'Registration Failed',
+        );
+      },
+      (success) {
+        state = state.copyWith(status: AuthStatus.unauthenticated);
+      },
+    );
+  }
+
+  // Login 
+  Future<void> login({
+    required String email,
+    required String password,
+  }) async {
+    state = state.copyWith(status: AuthStatus.loading);
+    final params = LoginUsecasesParams(email: email, password: password);
+    final result = await _loginUsecases.call(params);
+    result.fold(
+      (failure) {
+        state = state.copyWith(
+          status: AuthStatus.error,
+          errorMessage: 'Login Failed',
+        );
+      },
+      (authEntity) {
+        state = state.copyWith(
+          status: AuthStatus.authenticated,
+          authEntity: authEntity,
+        );
+      },
+    );
+  }
+}
