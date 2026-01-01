3ccac5f6c5e8175545e899fe5d9fddbacaa848be
nirajchand <149762449+nirajchand@users.noreply.github.com>
2026-01-01 10:30:28 +0545

    adding datasource

---
+++ lib/features/auth/data/datasources/auth_datasource.dart
@@ -0,0 +1,6 @@
+import 'package:farm_express/features/auth/data/models/auth_hive_model.dart';
+
+abstract interface class IAuthDatasource{
+  Future<bool> registerUser(AuthHiveModel user);
+  Future<AuthHiveModel?> loginUser(String email, String password);
+}
