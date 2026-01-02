b77201f0644104bd6c47b1defe48f9d1d722af0c
nirajchand <149762449+nirajchand@users.noreply.github.com>
2026-01-02 09:04:01 +0545

    adding final registration process

---
+++ lib/features/auth/presentation/pages/signup_screen.dart
@@ -0,0 +1,217 @@
+import 'package:farm_express/core/constants/colors.dart';
+import 'package:farm_express/features/auth/presentation/pages/login_screen.dart';
+import 'package:farm_express/features/auth/presentation/state/auth_state.dart';
+import 'package:farm_express/features/auth/presentation/view_model/auth_view_model.dart';
+import 'package:farm_express/widgets/elevated_button.dart';
+import 'package:farm_express/widgets/my_text_form_field.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class SignupScreen extends ConsumerStatefulWidget {
+  const SignupScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends ConsumerState<SignupScreen> {
+  final _formkey = GlobalKey<FormState>();
+  final TextEditingController _fullNameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+
+  Future<void> _register() async {
+    if (_formkey.currentState!.validate()) {
+      await ref
+          .read(authViewModelProvider.notifier)
+          .register(
+            fullName: _fullNameController.text,
+            email: _emailController.text,
+            password: _passwordController.text,
+            confirmPassword: _confirmPasswordController.text,
+          );
+    }
+  }
+
+
+  @override
+  void dispose() {
+    _fullNameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // auth State
+    final authState = ref.watch(authViewModelProvider);
+
+    ref.listen<AuthState>(authViewModelProvider, (previous, next) {
+      if (next.status == AuthStatus.error) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(next.errorMessage ?? 'Registration Failed'),
+            backgroundColor: Colors.red,
+          ),
+        );
+      }else if (next.status == AuthStatus.registered) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Registration Successful! Please login.'),
+            backgroundColor: Colors.green,
+          ),
+        );
+      }
+    });
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(backgroundColor: Colors.white),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Center(child: Image.asset("assets/images/project_logo.png")),
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(32),
+                boxShadow: [
+                  BoxShadow(
+                    color: const Color(0xFF000000).withValues(alpha: 0.12),
+                    offset: const Offset(0, 10),
+                    blurRadius: 30,
+                    spreadRadius: -5,
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Form(
+                  key: _formkey,
+                  child: Column(
+                    children: [
+                      Text(
+                        "Join Our Community",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: kGreenColor,
+                        ),
+                      ),
+                      Text(
+                        "Create your account",
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: kGreenColor,
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      MyTextFormField(
+                        controller: _fullNameController,
+                        labelText: "Full Name",
+                        prefixIcon: Icon(
+                          Icons.person_3_rounded,
+                          color: kPrimaryColor,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      MyTextFormField(
+                        controller: _emailController,
+                        labelText: "Email address",
+                        hint: Text(
+                          "e.g abc123@gmail.com",
+                          style: TextStyle(color: Colors.blueGrey),
+                        ),
+                        prefixIcon: Icon(Icons.email, color: kPrimaryColor),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your email';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(height: 15),
+                      MyTextFormField(
+                        controller: _passwordController,
+                        labelText: "Password",
+                        prefixIcon: Icon(Icons.lock, color: kPrimaryColor),
+                        suffixiocn: Icon(
+                          Icons.visibility_off,
+                          color: kPrimaryColor,
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your password';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(height: 15),
+                      MyTextFormField(
+                        controller: _confirmPasswordController,
+                        labelText: "Confirm Password",
+                        prefixIcon: Icon(Icons.lock, color: kPrimaryColor),
+                        suffixiocn: Icon(
+                          Icons.visibility_off,
+                          color: kPrimaryColor,
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please confirm your password';
+                          }
+                          if (value != _passwordController.text) {
+                            return 'Passwords do not match';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(height: 15),
+
+                      SizedBox(
+                        width: double.infinity,
+                        child: MyElevatedButton(
+                          onPressed: _register,
+                          backgroundColor: kPrimaryColor,
+                          foregroundColor: Colors.white,
+                          text: "Create Your Account",
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      TextButton(
+                        onPressed: () {
+                          setState(() {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => LoginScreen(),
+                              ),
+                            );
+                          });
+                        },
+                        child: Text(
+                          "Already have an account? SignIn",
+                          style: TextStyle(
+                            color: kPrimaryColor,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
