f8fce471f2af2776d3fb04027a21e9d40f2a47f5
nirajchand <149762449+nirajchand@users.noreply.github.com>
2026-01-15 20:37:21 +0545

    adding remote interface also

--- lib/features/auth/data/datasources/auth_datasource.dart
+++ lib/features/auth/data/datasources/auth_datasource.dart
@@ -1,6 +1,12 @@
+import 'package:farm_express/features/auth/data/models/auth_api_model.dart';
 import 'package:farm_express/features/auth/data/models/auth_hive_model.dart';
 
-abstract interface class IAuthDatasource{
-  Future<bool> registerUser(AuthHiveModel user);
+abstract interface class IAuthLocalDatasource {
+  Future<AuthHiveModel> registerUser(AuthHiveModel user);
   Future<AuthHiveModel?> loginUser(String email, String password);
+}
+
+abstract interface class IAuthRemoteDatasource {
+  Future<AuthApiModel> registerUser(AuthApiModel user);
+  Future<AuthApiModel?> loginUser(String email,String password);
 }
