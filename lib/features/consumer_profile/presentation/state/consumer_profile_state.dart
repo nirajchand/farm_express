07500c9178d8d95bdde8f68e276f5209528792e3
nirajchand <149762449+nirajchand@users.noreply.github.com>
2026-01-26 22:33:33 +0545

    adding state for profile

---
+++ lib/features/consumer_profile/presentation/state/consumer_profile_state.dart
@@ -0,0 +1,38 @@
+import 'package:equatable/equatable.dart';
+import 'package:farm_express/features/consumer_profile/domain/entities/profile_entity.dart';
+
+enum ConsumerProfileStatus {
+  initial,
+  loading,
+  loaded,
+  updating,
+  updated,
+  error,
+}
+
+class ConsumerProfileState extends Equatable {
+  final ConsumerProfileStatus status;
+  final ProfileEntity? profile;
+  final String? errorMessage;
+
+  const ConsumerProfileState({
+    this.status = ConsumerProfileStatus.initial,
+    this.profile,
+    this.errorMessage,
+  });
+
+  ConsumerProfileState copyWith({
+    ConsumerProfileStatus? status,
+    ProfileEntity? profile,
+    String? errorMessage,
+  }) {
+    return ConsumerProfileState(
+      status: status ?? this.status,
+      profile: profile ?? this.profile,
+      errorMessage: errorMessage ?? this.errorMessage,
+    );
+  }
+
+  @override
+  List<Object?> get props => [status, profile, errorMessage];
+}
