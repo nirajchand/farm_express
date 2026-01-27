84d8535f1d893c9f2741660831509aefaf58b5a4
nirajchand <149762449+nirajchand@users.noreply.github.com>
2026-01-27 20:43:45 +0545

    adding data layer implementaion

---
+++ lib/features/consumer/consumer_profile/data/datasources/consumer_profile_datasource.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+
+import 'package:farm_express/features/consumer/consumer_profile/data/models/profile_api_model.dart';
+
+abstract interface class IConsumerProfileRemoteDatasource {
+  Future<ProfileApiModel> getConsumerProfile();
+  Future<ProfileApiModel> updateProfile(ProfileApiModel data,File? image);
+}
