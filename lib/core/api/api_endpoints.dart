58e8c9a4123a99589b42c498df27063357802465
nirajchand <149762449+nirajchand@users.noreply.github.com>
2026-01-28 21:05:38 +0545

    adding endpoints in frontend

--- lib/core/api/api_endpoints.dart
+++ lib/core/api/api_endpoints.dart
@@ -1,13 +1,37 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+
 class ApiEndpoints {
   ApiEndpoints._();
 
-  static const String baseUrl = "http://10.0.2.2:3030/api/auth";
+  // static const String baseUrl = "http://10.0.2.2:2000/api";
+
+    // Configuration
+  static const bool isPhysicalDevice = false;
+  static const String _ipAddress = '192.168.1.1';
+  static const int _port = 2000;
+
+  // Base URLs
+  static String get _host {
+    if (isPhysicalDevice) return _ipAddress;
+    if (kIsWeb || Platform.isIOS) return 'localhost';
+    if (Platform.isAndroid) return '10.0.2.2';
+    return 'localhost';
+  }
+
+  static String get serverUrl => 'http://$_host:$_port';
+  static String get baseUrl => '$serverUrl/api';
+  static String get mediaServerUrl => serverUrl;
 
   static const Duration connectionTimeout = Duration(seconds: 30);
   static const Duration receiveTimeout = Duration(seconds: 30);
 
-
   // ==========Auth endpoints ======
-  static const String register = "/register";
-  static const String login = "/login";
+  static const String register = "/auth/register";
+  static const String login = "/auth/login";
+
+  // ================ consumerProfile=======================
+  static const String getProfile = "/consumer/getProfile";
+  static const String updateProfile = "/consumer/updateProfile";
 }
