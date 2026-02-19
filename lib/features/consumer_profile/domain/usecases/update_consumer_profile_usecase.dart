85069ae80df2fe33fae875c5bf24f42cd2a932f9
nirajchand <149762449+nirajchand@users.noreply.github.com>
2026-02-20 00:04:37 +0545

    changes in consumer profile

---
+++ lib/features/consumer_profile/domain/usecases/update_consumer_profile_usecase.dart
@@ -0,0 +1,54 @@
+import 'dart:io';
+
+import 'package:dartz/dartz.dart';
+import 'package:farm_express/core/error/failures.dart';
+import 'package:farm_express/core/usecases/app_usecases.dart';
+import 'package:farm_express/features/consumer_profile/data/repositories/consumer_profile_repository.dart';
+import 'package:farm_express/features/consumer_profile/domain/entities/profile_entity.dart';
+import 'package:farm_express/features/consumer_profile/domain/repositories/profile_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class UpdateConsumerProfileUsecaseParams {
+  final String? fullName;
+  final String? email;
+  final String? location;
+  final String? phoneNumber;
+  final String? profileImageUrl;
+  final File? profileImage;
+
+  UpdateConsumerProfileUsecaseParams({
+    this.fullName,
+    this.email,
+    this.location,
+    this.phoneNumber,
+    this.profileImageUrl,
+    this.profileImage,
+  });
+}
+
+final updateConsumerProfileUsecaseProvider =
+    Provider<UpdateConsumerProfileUsecase>((ref) {
+      return UpdateConsumerProfileUsecase(
+        iProfileRepository: ref.read(consumerProfileRepositoryProvider),
+      );
+    });
+
+class UpdateConsumerProfileUsecase
+    implements
+        UsecaseWithParams<ProfileEntity, UpdateConsumerProfileUsecaseParams> {
+  final IProfileRepository _iProfileRepository;
+
+  UpdateConsumerProfileUsecase({required IProfileRepository iProfileRepository})
+    : _iProfileRepository = iProfileRepository;
+  @override
+  Future<Either<Failure, ProfileEntity>> call(params) {
+    final entity = ProfileEntity(
+      fullName: params.fullName,
+      email: params.email,
+      location: params.location,
+      phoneNumber: params.phoneNumber,
+      profileImage: params.profileImageUrl,
+    );
+    return _iProfileRepository.updateUser(entity, params.profileImage);
+  }
+}
---
+++ lib/features/consumer_profile/presentation/view_model/consumer_profile_viewmodel.dart
@@ -0,0 +1,66 @@
+
+import 'package:farm_express/features/consumer_profile/domain/usecases/get_consumer_profile_usecases.dart';
+import 'package:farm_express/features/consumer_profile/domain/usecases/update_consumer_profile_usecase.dart';
+import 'package:farm_express/features/consumer_profile/presentation/state/consumer_profile_state.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final consumerProfileViewmodelProvider =
+    NotifierProvider<ConsumerProfileViewmodel, ConsumerProfileState>(
+      () => ConsumerProfileViewmodel(),
+    );
+
+class ConsumerProfileViewmodel extends Notifier<ConsumerProfileState> {
+  late final GetConsumerProfileUsecases _getConsumerProfileUsecases;
+  late final UpdateConsumerProfileUsecase _updateConsumerProfileUsecase;
+
+  @override
+  ConsumerProfileState build() {
+    _getConsumerProfileUsecases = ref.read(getConsumerProfileUsecasesProvider);
+    _updateConsumerProfileUsecase = ref.read(
+      updateConsumerProfileUsecaseProvider,
+    );
+    return ConsumerProfileState();
+  }
+
+  Future<void> getConsumerUser() async {
+    state = state.copyWith(status: ConsumerProfileStatus.loading);
+    final result = await _getConsumerProfileUsecases.call();
+    result.fold(
+      (failure) {
+        state = state.copyWith(
+          status: ConsumerProfileStatus.error,
+          errorMessage: failure.message,
+        );
+      },
+      (profileData) {
+        state = state.copyWith(
+          status: ConsumerProfileStatus.loaded,
+          profile: profileData,
+        );
+      },
+    );
+  }
+
+  Future<void> updateProfile(UpdateConsumerProfileUsecaseParams data) async {
+    state = state.copyWith(status: ConsumerProfileStatus.loading);
+
+    final result = await _updateConsumerProfileUsecase.call(data);
+
+    result.fold(
+      (failure) {
+        state = state.copyWith(
+          status: ConsumerProfileStatus.error,
+          errorMessage: failure.message,
+        );
+      },
+      (success) async {
+        state = state.copyWith(
+          status: ConsumerProfileStatus.updated,
+          profile: success,
+        );
+
+        await getConsumerUser();
+      },
+    );
+  }
+}
